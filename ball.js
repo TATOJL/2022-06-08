7cbad03ba439b7b9dc900149cb84044501749179
帳號 <>
2022-06-08 20:54:51 +0800

    .

--- ball.js
+++
@@ -1,9 +0,0 @@
-let run = document.querySelector('#run-btn');
-let ball = document.querySelector('#run-ball');
-
-if (run && ball) {
-    run.addEventListener('click', () => {
-        console.log('bbbbb');
-        // 球體向左移動 80%。
-    })
-}
---
+++ js/ball.js
@@ -0,0 +1,9 @@
+let run = document.querySelector('#run-btn');
+let ball = document.querySelector('#run-ball');
+
+if (run && ball) {
+    run.addEventListener('click', () => {
+        console.log('bbbbb');
+        // 球體向左移動 80%。
+    })
+}
