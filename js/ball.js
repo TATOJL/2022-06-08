b99466fca5e3e9766d966d27dccd0d170619381e
OHO <74269546+TATOJL@users.noreply.github.com>
2022-06-08 21:23:10 +0800

    Add files via upload

--- js/ball.js
+++ js/ball.js
@@ -15,3 +15,46 @@ if (run && ball) {
         }
     })
 }
+
+// dataset
+ball.dataset.id = 9999;
+
+console.log(ball.dataset);
+
+console.log(ball.dataset.id);
+
+// 找爸爸
+let body = getBody(run);
+console.log(body);
+
+
+function getBody(dom) {
+    while (dom.tagName != 'BODY' && dom) {
+        dom = dom.parentNode
+    }
+    return dom;
+}
+
+// 找兒子
+console.log(body.childNodes);
+
+let children = body.children;
+
+for (let i in children) {
+    let cdom = children[i];
+    console.log(cdom);
+}
+
+
+
+let todo = document.querySelector('#todo');
+let newLi = document.createElement('li');
+
+// 異動內容
+newLi.innerHTML = "I'm New Li";
+
+// 新增兒子
+todo.appendChild(newLi);
+
+// 刪除兒子
+todo.removeChild(newLi); // 等同 newLi.remove()
